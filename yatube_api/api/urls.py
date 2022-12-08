22b2b9772d2f7c164e062aacc9371af6b2f82d2a
Мария Лапикова <mashik_p@mail.ru>
2022-12-08 21:13:44 +0300

    update api urls and settings

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,27 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+	PostViewSet,
+	GroupViewSet,
+	CommentViewSet,
+	FollowViewSet
+)
+
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register('groups', GroupViewSet, basename='groups')
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router_v1.register('groups', FollowViewSet, basename='followings')
+
 
 urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ]
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
+    'djoser',
     'api',
     'posts',
 ]
@@ -90,4 +92,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
+
+SIMPLE_JWT = {
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+   'AUTH_HEADER_TYPES': ('Bearer',),
 }
