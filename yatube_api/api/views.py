abc1462c7c9c3010e0e13706c120d4976d829802
Мария Лапикова <mashik_p@mail.ru>
2022-12-10 00:45:58 +0300

    update readme

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, mixins, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .models import Post, Group, Comment, Follow
+from posts.models import Post, Group, Comment, Follow
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     PostSerializer,
